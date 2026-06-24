bc95b207ffb6512a74f99cd949d3ef502b1cec58
Jonaxrd <jr014742@gmail.com>
2026-06-24 01:30:28 -0600

    test pass

--- tests/test_celsius_a_fahrenheit.py
+++ tests/test_celsius_a_fahrenheit.py
@@ -1,15 +1,9 @@
-import pytest
 from celsius_a_fahrenheit import celsius_a_fahrenheit
 
 
-def test_conversion_correcta():
-    assert celsius_a_fahrenheit(25) == 77
-
-
-def test_caso_limite_cero_celsius():
+def test_conversion_zero():
     assert celsius_a_fahrenheit(0) == 32
 
 
-def test_error_con_texto():
-    with pytest.raises(TypeError):
-        celsius_a_fahrenheit("veinte")
+def test_conversion_100():
+    assert celsius_a_fahrenheit(100) == 212
