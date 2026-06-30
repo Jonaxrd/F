151e11c556609a67e035e32f6c70e3d3b03001e2
Jonaxrd <jr014742@gmail.com>
2026-06-30 09:29:38 -0600

    Prueba fail

--- tests/test_celsius_a_fahrenheit.py
+++ tests/test_celsius_a_fahrenheit.py
@@ -2,7 +2,7 @@ from celsius_a_fahrenheit import celsius_a_fahrenheit
 
 
 def test_conversion_zero():
-    assert celsius_a_fahrenheit(0) == 32
+    assert celsius_a_fahrenheit(0) == 30
 
 
 def test_conversion_100():
